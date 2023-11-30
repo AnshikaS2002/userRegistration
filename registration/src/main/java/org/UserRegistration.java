3e8b0465e242da7ec0c4177f644c61ccd29d6582
Anshika Semwal <semwal.anshi.10@gmail.com>
2023-12-01 00:42:08 +0530

    updated validatePassword to check for at least one special char

--- registration/src/main/java/org/UserRegistration.java
+++ registration/src/main/java/org/UserRegistration.java
@@ -53,7 +53,7 @@ public class UserRegistration {
     }
 
     void validatePassword(String pass) {
-        String passRegex = "^(?=.*[A-Z])(?=.*\\d).*$";
+        String passRegex = "^(?=.*[A-Z])(?=.*\\d).(?=.*\\W).*$";
         Pattern pattern = Pattern.compile(passRegex);
         Matcher matcher = pattern.matcher(pass);
 
